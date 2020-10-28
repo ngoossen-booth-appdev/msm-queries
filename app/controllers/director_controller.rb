c560edbf6a710f7586a985d8c1168f43b7ec2dd5
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-28 21:49:46 +0000

    more progress

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -1,6 +1,32 @@
 class DirectorController < ActionController::Base
   def index
+    @list_of_directors = Director.all
+
+
     render({ :template => "director_templates/index.html.erb"})
   end
+
+  def details
+    @director_id = params.fetch("director_id")
+
+    render({ :template => "director_templates/director_details.html.erb"})
+
+  end
+
+  def eldest
+    # @director_id = params.fetch("director_id")
+
+    render({ :template => "director_templates/director_eldest.html.erb"})
+
+  end
+
+  def youngest
+    # @director_id = params.fetch("director_id")
+
+    render({ :template => "director_templates/director_youngest.html.erb"})
+
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
   get("/directors", {:controller => "director", :action => "index"})
+  get("/directors/:director_id", {:controller => "director", :action => "details"})
+  get("/directors/eldest", {:controller => "director", :action => "eldest"})
+  get("/directors/youngest", {:controller => "director", :action => "youngest"})
+  
+  
+  
   get("/actors", {:controller => "actor", :action => "index"})
   get("/characters", {:controller => "character", :action => "index"})
   get("/movies", {:controller => "movie", :action => "index"})
